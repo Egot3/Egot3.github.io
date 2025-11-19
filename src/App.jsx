7aca57b91aadbc65f1751a7c9f4831ed24b14325
Egor <tihonovegor09@gmail.com>
2025-11-19 23:33:03 +0300

    flag planted

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import Accordion from 'react-bootstrap/Accordion';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios, { Axios } from 'axios';
 
 function App() {
-  const colCount = 2
-  const rowCount = 2
+  const [repos, setRepos] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  const githubUsername = 'Egot3'
+  const apiUrl = `https://api.github.com/users/${githubUsername}/repos`
+
+  useEffect(()=>{
+    const pullRepos = async () => {
+    try{
+      const response = await axios.get(apiUrl)
+      
+      console.log(response)
+    }
+
+    catch(err){
+      setLoading(err.message)
+    }
+
+    finally{
+      setLoading(false)
+    }
+  }
+  pullRepos()
+  }, apiUrl)
+
   return (
-    <Accordion className='w-100'>
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+    <>
+
+    </>
   )
 }
 
